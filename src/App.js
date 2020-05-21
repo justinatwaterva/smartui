7b40cc133c834663da5df0d45560940cf6b751a6
justinatwaterva <justin.atwater@va.gov>
2020-05-21 10:16:12 -0400

    Added fetch GET request to FHIR Authorization

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
 
   componentDidMount() {
     const self = this;
-	console.log("Test for Env Variables: " + process.env.REACT_APP_URL);
+  	console.log("Test for Env Variables: " + process.env.REACT_APP_URL);
     FHIR.oauth2.ready(function(smart){
       smart.patient.read().then(function(pt) {
         //self.setState({ patientName: pt.name[0].text });
@@ -20,7 +20,44 @@ class App extends React.Component {
           </ul>});
       });
 	  self.setState({ patientName: smart.state.tokenResponse.access_token });
+    }).then(function() {
+      self.getPatients();
     });
+  }
+
+  getPatients() {
+    const self = this;
+    let fullURL = this.getServerURL() + "/Patient/" + this.getPatientId(); 
+    fetch(fullURL, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + this.getAccessToken()
+      }
+    }).then(data => {
+      self.setState({ fhir_response: "Status: " + data.status });
+    });
+  }
+  
+  getAccessToken() {
+    let sessionId = sessionStorage.getItem("SMART_KEY");
+    let sessionObj = sessionId ? sessionStorage.getItem(sessionId.replace(/["']/g, "")) : null;
+    let token = sessionObj ? JSON.parse(sessionObj).tokenResponse.access_token : '';
+    return token;
+  }
+
+  getPatientId() {
+    let sessionId = sessionStorage.getItem("SMART_KEY");
+    let sessionObj = sessionId ? sessionStorage.getItem(sessionId.replace(/["']/g, "")) : null;
+    let patientId = sessionObj ? JSON.parse(sessionObj).tokenResponse.patient : '';
+    return patientId;
+  }
+
+  getServerURL() {
+    let sessionId = sessionStorage.getItem("SMART_KEY");
+    let sessionObj = sessionId ? sessionStorage.getItem(sessionId.replace(/["']/g, "")) : null;
+    let url = sessionObj ? JSON.parse(sessionObj).serverUrl : '';
+    return url;
   }
 
   render() {
@@ -29,6 +66,8 @@ class App extends React.Component {
 		<TestAlert />
         {this.state && this.state.patientName }
         {this.state && this.state.patientInfo }
+        <br></br>
+        {this.state && this.state.fhir_response}
       </div>
     );
   }
