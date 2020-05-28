13e94a70870d0728f9f4765ee78875f20c5e0239
justinatwaterva <justin.atwater@va.gov>
2020-05-28 10:06:31 -0400

    updated fetch

--- src/App.js
+++ src/App.js
@@ -8,24 +8,26 @@ class App extends React.Component {
     const self = this;
   	console.log("Test for Env Variables: " + process.env.REACT_APP_URL);
     FHIR.oauth2.ready(function(smart){
-      smart.patient.read().then(function(pt) {
-        //self.setState({ patientName: pt.name[0].text });
-        self.setState({ patientInfo: 
-          <ul>
-            <li>ID: { pt.id }</li>
-            <li>Gender: { pt.gender }</li>
-            <li>Birthdate: { pt.birthDate }</li>
-            <li>Marital Status: { pt.maritalStatus && pt.maritalStatus.text }</li>
-            <li>Address: { pt.address && pt.address[0].text }</li>
-          </ul>});
-      });
-	  self.setState({ patientName: smart.state.tokenResponse.access_token });
-    }).then(function() {
-      self.getPatients();
+        /*
+        smart.patient.read().then(function(pt) {
+          //self.setState({ patientName: pt.name[0].text });
+          self.setState({ patientInfo: 
+            <ul>
+              <li>ID: { pt.id }</li>
+              <li>Gender: { pt.gender }</li>
+              <li>Birthdate: { pt.birthDate }</li>
+              <li>Marital Status: { pt.maritalStatus && pt.maritalStatus.text }</li>
+              <li>Address: { pt.address && pt.address[0].text }</li>
+            </ul>});
+        });
+        self.setState({ patientName: smart.state.tokenResponse.access_token });
+        */
+
+        self.getPatients();
     });
   }
 
-  getPatients() {
+  xgetPatients() {
     const self = this;
     let fullURL = this.getServerURL() + "/Patient/" + this.getPatientId(); 
     fetch(fullURL, {
@@ -38,7 +40,30 @@ class App extends React.Component {
       self.setState({ fhir_response: "Status: " + data.status });
     });
   }
-  
+
+  getPatients() {
+    const self = this;
+    //let fullURL = this.getServerURL() + "/Patient/" + this.getPatientId(); 
+    let fullURL = "http://localhost:8080/rating-service/"+ this.getPatientId()+ "/read"
+    fetch(fullURL, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'token':  this.getAccessToken(),
+        'Access-Control-Allow-Methods': 'DELETE, GET, POST, PUT, OPTIONS, HEAD',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Expose-Headers': 'ETag, Content-Location, Location, X-Request-Id, WWW-Authenticate, Date',
+        'Access-Control-Max-Age': '0'        
+      }
+    }).then(response => response.json())
+    .then(response => {
+      //self.setState({ rating_response: "Status: " + data.status });
+      self.setState({ ratingdata:  JSON.stringify(response) });
+      return response;
+    });
+  } 
+ 
+
   getAccessToken() {
     let sessionId = sessionStorage.getItem("SMART_KEY");
     let sessionObj = sessionId ? sessionStorage.getItem(sessionId.replace(/["']/g, "")) : null;
@@ -67,7 +92,7 @@ class App extends React.Component {
         {this.state && this.state.patientName }
         {this.state && this.state.patientInfo }
         <br></br>
-        {this.state && this.state.fhir_response}
+        {this.state && this.state.ratingdata}
       </div>
     );
   }
